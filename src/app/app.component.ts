a9d060085dcde43fdd8ed4c8d888af8e24628109
Alexis Mercado <leo-alexis@hotmail.com>
2022-02-01 18:44:54 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+//DECORADOR
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html' 
+  /*
+  template: '<span>Alexis</span>' 
+  template: `
+  <h1>Hola de nuevo</h1>
+  `*/
 })
+//el templateUrl ayuda a definir un path para el archivo html 
+
 export class AppComponent {
-  title = 'bases';
+  
 }
